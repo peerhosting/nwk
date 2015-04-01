7572b41338e412fe594ddad953c65c43e41b65de
Graham Baker <graham.cr.baker@gmail.com>
2015-03-31 20:47:08 -0700

    progress

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,18 @@
 module.exports = function(grunt) {
 
 // yesss
-grunt.initConfig({
-    nodewebkit: {
-        options: {
-            platforms: ['win','osx'],
-            buildDir: './builds',
-            macIcns: 'myIcons.icns',
-            winIco: 'myIcons.ico',
-            macZip: false
-        },
-        src: ['./example/public/**/*']
-    }
-})
+// grunt.initConfig({
+//     nodewebkit: {
+//         options: {
+//             platforms: ['win','osx'],
+//             buildDir: './builds',
+//             macIcns: 'myIcons.icns',
+//             winIco: 'myIcons.ico',
+//             macZip: false
+//         },
+//         src: ['./example/public/**/*']
+//     }
+// })
 
 
 
