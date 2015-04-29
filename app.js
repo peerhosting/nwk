02c1ac08e3e1d37ceac7dc9c79a47e63c4547aa9
Graham Baker <graham.cr.baker@gmail.com>
2015-04-28 21:19:03 -0700

    getting up to date

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var browse = require('./lib/browse');
 var upload = require('./lib/upload');
 var navigate = require('./lib/navigate');
 var files = require('./lib/files');
+var update = require('./lib/update');
 // console.log(path.join(__dirname, './local'));
 app.use(express.static(path.join(__dirname, './lib/local')));
 
@@ -13,6 +14,7 @@ app.use(home);
 app.use(browse);
 app.use(upload);
 app.use(navigate);
+app.use(update);
 app.use(files);
 
 
